29fe32ae5135f56bd17ff2f994d73647e64aa22b
Isaiahsylnol <pictureisaiah@gmail.com>
2023-01-10 21:55:49 -0500

    Code cleanup on frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import styled from "styled-components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import AuthService from "./services/auth.service";
@@ -14,12 +13,6 @@ import { useQuery } from "@apollo/client";
 import "./App.css";
 import Footer from "./components/Footer";
 
-const Wrapper = styled.div`
-  font-size: 1.5em;
-  text-align: center;
-  height: screen;
-`;
-
 function App() {
   const { loading, error, data } = useQuery(GET_MOVIES);
 
@@ -28,25 +21,23 @@ function App() {
   if (error) return <h2>Error fetching movie data!</h2>;
 
   return (
-    <Wrapper>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" exact element={<ListMovies />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          {data?.movies.map((movie, i) => (
-            <Route
-              exact
-              key={`route${i}`}
-              path={`/detail/${movie._id}`}
-              element={<MovieDetail movie={movie} />}
-            />
-          ))}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Router>
-    </Wrapper>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" exact element={<ListMovies />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        {data?.movies.map((movie, i) => (
+          <Route
+            exact
+            key={`route${i}`}
+            path={`/detail/${movie._id}`}
+            element={<MovieDetail movie={movie} />}
+          />
+        ))}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
