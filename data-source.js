d2d0cc4979a8a8c7f5dfce8916a399dc23f76931
Isaiahsylnol <pictureisaiah@gmail.com>
2023-10-04 14:59:42 -0400

    Data source update

--- data-source.js
+++ data-source.js
@@ -2,8 +2,8 @@ const typeorm = require("typeorm");
 const MovieEntity = require("./entity/movie.entity");
 
 const dataSource = new typeorm.DataSource({
-  type: "postgres",
-  url: process.env.DATABASE_URL,
+  type: `postgres`,
+  url: `${process.env.DATABASE_URL}`,
   entities: [MovieEntity],
 });
 
