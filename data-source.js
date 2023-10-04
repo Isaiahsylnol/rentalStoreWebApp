ed70c7896e6ae4845dbfec2984d21d8dd20cf4aa
Isaiahsylnol <pictureisaiah@gmail.com>
2023-10-04 14:21:31 -0400

    Updated data source

--- data-source.js
+++ data-source.js
@@ -3,11 +3,7 @@ const MovieEntity = require("./entity/movie.entity");
 
 const dataSource = new typeorm.DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  url: process.env.DATABASE_URL,
   entities: [MovieEntity],
 });
 
